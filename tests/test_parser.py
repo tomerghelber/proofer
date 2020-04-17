78774ac057dcec10714c32ccd8e99851e835ccff
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-04-17 20:04:11 +0000

    Update test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -16,7 +16,7 @@ def nCk(n: int, k: int) -> float:
 
 
 @pytest.mark.parametrize("number_of_points", range(2, 12))
-def test_parse_line(number_of_points):
+def test_parse_line(number_of_points: int):
     expected_number_of_lines = nCk(number_of_points, 2)
     expected_number_of_angles = nCk(number_of_points, 3)
     expected_number_of_objects = number_of_points + expected_number_of_lines + expected_number_of_angles
@@ -27,21 +27,21 @@ def test_parse_line(number_of_points):
 
 
 @pytest.mark.parametrize("number_of_points", range(2))
-def test_parse_line_not_enough_points(number_of_points):
+def test_parse_line_not_enough_points(number_of_points: int):
     with pytest.raises(ValueError):
         parse_line('line ' + ','.join(map(str, range(number_of_points))))
 
 
 @pytest.mark.parametrize("number_of_points", range(3, 12))
-def test_parse_polygon(number_of_points):
+def test_parse_polygon(number_of_points: int):
     parsed = parse_line('polygon ' + ','.join(map(str, range(number_of_points))))
 
-    expected_number_of_objects = 3 * len(number_of_points)
+    expected_number_of_objects = 3 * number_of_points
 
     assert expected_number_of_objects == len(parsed)
 
 
 @pytest.mark.parametrize("number_of_points", range(3))
-def test_parse_polygon_not_enough_points(number_of_points):
+def test_parse_polygon_not_enough_points(number_of_points: int):
     with pytest.raises(ValueError):
         parse_line('polygon ' + ','.join(map(str, range(number_of_points))))
