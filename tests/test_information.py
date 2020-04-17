06a24142dd830c824e99a0ddc4244ed0e0bd60c6
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-04-17 20:39:24 +0000

    Update test_information.py

--- tests/test_information.py
+++ tests/test_information.py
@@ -24,7 +24,38 @@ def test_sqlalchemy_information_insert_line(tested_sqlalchemy_information):
     tested_sqlalchemy_information.execute(rule)
 
 
-def test_sqlalchemy_information_double_insert_line(tested_sqlalchemy_information):
+def test_sqlalchemy_information_insert_line_point1_expected(tested_sqlalchemy_information):
+    class DummyRule:
+        def execute(self, session):
+            session.add(Line(point2='B'))
+            session.commit()
+
+    rule = DummyRule()
+    tested_sqlalchemy_information.execute(rule)
+
+
+def test_sqlalchemy_information_insert_line_point2_expected(tested_sqlalchemy_information):
+    class DummyRule:
+        def execute(self, session):
+            session.add(Line(point2='B'))
+            session.commit()
+
+    rule = DummyRule()
+    tested_sqlalchemy_information.execute(rule)
+
+
+def test_sqlalchemy_information_insert_angle_size_negative(tested_sqlalchemy_information):
+    class DummyRule:
+        def execute(self, session):
+            session.add(Angle(point1='A', point2='B', size=-1))
+            session.commit()
+
+    rule = DummyRule()
+    with pytest.raises(IntegrityError):
+        tested_sqlalchemy_information.execute(rule)
+
+
+def test_sqlalchemy_information_double_insert_line_fails(tested_sqlalchemy_information):
     class DummyRule:
         def execute(self, session):
             session.add(Line(point1='A', point2='B'))
@@ -34,7 +65,6 @@ def test_sqlalchemy_information_double_insert_line(tested_sqlalchemy_information
     tested_sqlalchemy_information.execute(rule)
     with pytest.raises(IntegrityError):
         tested_sqlalchemy_information.execute(rule)
-
 
 
 def test_sqlalchemy_information_insert_angle(tested_sqlalchemy_information):
@@ -47,7 +77,59 @@ def test_sqlalchemy_information_insert_angle(tested_sqlalchemy_information):
     tested_sqlalchemy_information.execute(rule)
 
 
-def test_sqlalchemy_information_double_insert_angle(tested_sqlalchemy_information):
+def test_sqlalchemy_information_insert_angle_point1_expected(tested_sqlalchemy_information):
+    class DummyRule:
+        def execute(self, session):
+            session.add(Angle(angle_point='B', point2='C'))
+            session.commit()
+
+    rule = DummyRule()
+    tested_sqlalchemy_information.execute(rule)
+
+
+def test_sqlalchemy_information_insert_angle_angle_point_expected(tested_sqlalchemy_information):
+    class DummyRule:
+        def execute(self, session):
+            session.add(Angle(point1='A', point2='C'))
+            session.commit()
+
+    rule = DummyRule()
+    tested_sqlalchemy_information.execute(rule)
+
+
+def test_sqlalchemy_information_insert_angle_point2_expected(tested_sqlalchemy_information):
+    class DummyRule:
+        def execute(self, session):
+            session.add(Angle(point1='A', angle_point='B'))
+            session.commit()
+
+    rule = DummyRule()
+    tested_sqlalchemy_information.execute(rule)
+
+
+def test_sqlalchemy_information_insert_angle_size_negative(tested_sqlalchemy_information):
+    class DummyRule:
+        def execute(self, session):
+            session.add(Angle(point1='A', angle_point='B', point2='C', size=-1))
+            session.commit()
+
+    rule = DummyRule()
+    with pytest.raises(IntegrityError):
+        tested_sqlalchemy_information.execute(rule)
+
+
+def test_sqlalchemy_information_insert_angle_size_too_big(tested_sqlalchemy_information):
+    class DummyRule:
+        def execute(self, session):
+            session.add(Angle(point1='A', angle_point='B', point2='C', size=-361))
+            session.commit()
+
+    rule = DummyRule()
+    with pytest.raises(IntegrityError):
+        tested_sqlalchemy_information.execute(rule)
+
+
+def test_sqlalchemy_information_double_insert_angle_fails(tested_sqlalchemy_information):
     class DummyRule:
         def execute(self, session):
             session.add(Angle(point1='A', angle_point='B', point2='C'))
