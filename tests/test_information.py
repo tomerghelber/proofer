047b704b79b31148a6f3a3060e932a1bb7f89d61
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-04-17 19:43:38 +0000

    Update test_information.py

--- tests/test_information.py
+++ tests/test_information.py
@@ -1,12 +1,22 @@
+from sqlalchemy import create_engine
+
 from proofer.informations import SqlAlchemyInformation, Line, Angle
 
-def test_sqlalchemy_information_insert_line():
-    sqlalchemy_information = SqlAlchemyInformation()
+@pytest.fixture
+def memory_engine():
+    return create_engine('sqlite:///:memory:', echo=True)
 
+
+@pytest.fixture
+def tested_sqlalchemy_information(memory_engine):
+    return SqlAlchemyInformation(memory_engine)
+
+
+def test_sqlalchemy_information_insert_line(tested_sqlalchemy_information):
     class DummyRule:
         def execute(self, session):
             line = Line('A', 'B')
             session.add(line)
 
     rule = DummyRule()
-    sqlalchemy_information.execute(rule)
+    tested_sqlalchemy_information.execute(rule)
