9cc09e5a2ab55351253e640497393183610dd607
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-04-17 21:53:53 +0000

    Update test_rules.py

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -20,12 +20,12 @@ def test_SumAngles(sqlalchemy_information):
     size2 = 60
     with engine.connect() as connection:
         middle_point = 'D'
-        connection.execute(Angle.insert(), {'point1': point1      , 'angle_point': angle_point, 'point2': middle_point, 'size': size1}
-        connection.execute(Angle.insert(), {'point1': middle_point, 'angle_point': angle_point, 'point2': point2      , 'size': size2}
+        connection.execute(Angle.insert(), {'point1': point1      , 'angle_point': angle_point, 'point2': middle_point, 'size': size1})
+        connection.execute(Angle.insert(), {'point1': middle_point, 'angle_point': angle_point, 'point2': point2      , 'size': size2})
     
     tested_rule = SumAngles()
     
     sqlalchemy_information.execute(tested_rule)
     with engine.connect() as connection:
-        result = connection.execute(Angle.select(), {'point1': point1, 'angle_point': angle_point, 'point2': point2, 'size': size1 + sizes}
+        result = connection.execute(Angle.select(), {'point1': point1, 'angle_point': angle_point, 'point2': point2, 'size': size1 + sizes})
         assert 1 == len(result)
