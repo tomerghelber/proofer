5a02a8fe523e71ca316671190e3aff9ed6488e51
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-04-17 20:29:19 +0000

    Update test_information.py

--- tests/test_information.py
+++ tests/test_information.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 import pytest
 
 from proofer.informations import SqlAlchemyInformation, Line, Angle
@@ -17,6 +18,7 @@ def test_sqlalchemy_information_insert_line(tested_sqlalchemy_information):
     class DummyRule:
         def execute(self, session):
             session.add(Line(point1='A', point2='B'))
+            session.commit()
 
     rule = DummyRule()
     tested_sqlalchemy_information.execute(rule)
@@ -26,6 +28,7 @@ def test_sqlalchemy_information_insert_angle(tested_sqlalchemy_information):
     class DummyRule:
         def execute(self, session):
             session.add(Angle(point1='A', angle_point='B', point2='C'))
+            session.commit()
 
     rule = DummyRule()
     tested_sqlalchemy_information.execute(rule)
@@ -35,8 +38,8 @@ def test_sqlalchemy_information_double_insert_angle(tested_sqlalchemy_informatio
         def execute(self, session):
             session.add(Angle(point1='A', angle_point='B', point2='C'))
             session.commit()
-            session.add(Angle(point1='A', angle_point='B', point2='C'))
-            session.commit()
 
     rule = DummyRule()
-    tested_sqlalchemy_information.execute(rule)
+    with pytest.raises(IntegrityError)
+        tested_sqlalchemy_information.execute(rule)
+        tested_sqlalchemy_information.execute(rule)
