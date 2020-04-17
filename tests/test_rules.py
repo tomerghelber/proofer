c18de001c012b1f2c6073f45352a5b548720021c
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-04-17 22:31:10 +0000

    Update test_rules.py

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,4 +1,7 @@
+from contextlib import closing
+
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from proofer.informations import SqlAlchemyInformation, Line, Angle
 from proofer.rules import SumAngles
@@ -10,26 +13,26 @@ import pytest
 def memory_engine():
     return create_engine('sqlite:///:memory:')
 
+@pytest.fixture
+def memory_session(memory_engine):
+    with closing(sessionmaker(memory_engine)()) as session:
+        yield session
 
 @pytest.fixture
 def sqlalchemy_information(memory_engine):
     return SqlAlchemyInformation(memory_engine)
 
 
-def test_SumAngles(sqlalchemy_information, memory_engine):
-    point1 = 'A'
-    angle_point = 'B'
-    point2 = 'C'
-    size1 = 30
-    size2 = 60
-    with memory_engine.connect() as connection:
-        middle_point = 'D'
-        connection.execute(Angle.insert(), {'point1': point1      , 'angle_point': angle_point, 'point2': middle_point, 'size': size1})
-        connection.execute(Angle.insert(), {'point1': middle_point, 'angle_point': angle_point, 'point2': point2      , 'size': size2})
+def test_SumAngles(sqlalchemy_information, memory_session):
+    angle1 = Angle(point1='A', angle_point='B', point2 = 'D', size=30)
+    angle2 = Angle(point2=angle1.point2, angle_point=angle1.angle_point, point2='C', size=60)
+    
+    memory_session.add_all([angle1, angle2])
+    memory_session.commit()
     
     tested_rule = SumAngles()
     
     sqlalchemy_information.execute(tested_rule)
-    with memory_engine.connect() as connection:
-        result = connection.execute(Angle.select(), {'point1': point1, 'angle_point': angle_point, 'point2': point2, 'size': size1 + size2})
-        assert 1 == len(result)
+    
+    result = memory_session.query(Angle).get([angle1.point1, angle1.angle_point, angle2.point2])
+    assert result.size == angle1.size + angle2.size
