8dac919053f8209dffba1b8381b910035e3b8d38
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-04-17 21:56:24 +0000

    Update test_rules.py

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,5 +1,9 @@
+from sqlalchemy import create_engine
+
 from proofer.informations import SqlAlchemyInformation, Line, Angle
 from proofer.rules import SumAngles
+
+import pytest
 
 
 @pytest.fixture
@@ -12,13 +16,13 @@ def sqlalchemy_information(memory_engine):
     return SqlAlchemyInformation(memory_engine)
 
 
-def test_SumAngles(sqlalchemy_information):
+def test_SumAngles(sqlalchemy_information, memory_engine):
     point1 = 'A'
     angle_point = 'B'
     point2 = 'C'
     size1 = 30
     size2 = 60
-    with engine.connect() as connection:
+    with memory_engine.connect() as connection:
         middle_point = 'D'
         connection.execute(Angle.insert(), {'point1': point1      , 'angle_point': angle_point, 'point2': middle_point, 'size': size1})
         connection.execute(Angle.insert(), {'point1': middle_point, 'angle_point': angle_point, 'point2': point2      , 'size': size2})
@@ -26,6 +30,6 @@ def test_SumAngles(sqlalchemy_information):
     tested_rule = SumAngles()
     
     sqlalchemy_information.execute(tested_rule)
-    with engine.connect() as connection:
+    with memory_engine.connect() as connection:
         result = connection.execute(Angle.select(), {'point1': point1, 'angle_point': angle_point, 'point2': point2, 'size': size1 + sizes})
         assert 1 == len(result)
