5ca47702afcfa8cc67dad8bb5d387be7d85d84b4
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-04-17 21:30:05 +0000

    Update rules.py

--- proofer/rules.py
+++ proofer/rules.py
@@ -2,7 +2,10 @@ from abc import ABC, abstractmethod
 from itertools import chain
 import typing
 
-from informations import Information, Angle, Line
+from sqlalchemy import and_
+from sqlalchemy.orm import Query
+
+from informations import Angle, Line
 
           
 class Rule(ABC):
@@ -14,15 +17,27 @@ class Rule(ABC):
 class SimpleRule(Rule):
     def __init__(self, query: set, name: typing.Optional[str] = None):
         self.__query = query
-        self.__name = name
+        self.__name = "SimpleRule" if name is None else name
 
-    def execute(self, information: Information):
-        for result in information.find_all(self.__query):
-            yield self.__conclusion(result)
+    def execute(self, session):
+        self.__query.with_session(session)
 
+    def __repr__(self):
+        return "{}({})".format(self.__name, str(self.__query)
 
-# Sum angles
-# INESET INTO angles (point1, angle_point, point2, size)
-# SELECT angles1.point1 as point1, angles1.angle_point as angle_point, angles2.point2 as point2, angles1.size + angles2.size as size
-# FROM angles angles1, angles angles2
-# WHERE angles1.angle_point == angles2.angle_point AND angles1.point2 == angles2.point1;
+def SumAngles():
+    """Sum angles.
+    INESET INTO angles (point1, angle_point, point2, size)
+    SELECT angles1.point1 as point1, angles1.angle_point as angle_point, angles2.point2 as point2, angles1.size + angles2.size as size
+    FROM angles angles1, angles angles2
+    WHERE angles1.angle_point == angles2.angle_point AND angles1.point2 == angles2.point1;
+    """
+    first_angle = aliased(Angle)
+    second_angle = aliased(Angle)
+    query = Angle.insert().from_select(
+        Query([first_angle.point1, first_angle.angle_point, second_angle.point2, angles1.size + angles2.size])
+            select_from(first_angle)
+            .join(second_angle, and_(first_angle.angle_point == second_angle.angle_point, first_angle.point2 == second_angle.point1))
+            .filter()
+    )
+    return SimpleRule(query, "SimpleRule")
