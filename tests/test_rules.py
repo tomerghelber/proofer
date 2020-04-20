ae4cf0f8dd9811ef2927ca4477af977d27ba8b0a
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-04-20 18:53:28 +0000

    Update test_rules.py
    Added test_SumLines

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from proofer.informations import SqlAlchemyInformation, Line, Angle
-from proofer.rules import SumAngles
+from proofer.rules import SumAngles, SumLines
 
 import pytest
 
@@ -36,3 +36,18 @@ def test_SumAngles(sqlalchemy_information, memory_session):
     
     result = memory_session.query(Angle).get([angle1.point1, angle1.angle_point, angle2.point2])
     assert result.size == angle1.size + angle2.size
+
+def test_SumLines(sqlalchemy_information, memory_session):
+    angle = Angle(point1='A', angle_point='B', point2='C', size=180)
+    line1 = Angle(point1=angle.point1, point2=angle.angle_point, size=2)
+    line2 = Angle(point1=angle.angle_point, point2=angle.point2, size=3)
+    
+    memory_session.add_all([angle, line1, line2])
+    memory_session.commit()
+    
+    tested_rule = SumLines()
+
+    sqlalchemy_information.execute(tested_rule)
+    
+    result = memory_session.query(Line).get([line1.point1, line2.line2])
+    assert result.size == line1.size + line2.size
